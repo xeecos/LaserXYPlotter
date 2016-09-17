338f5b015cfd3e102b8a1c669135f0f72b2b4f49
xeecos <flashindream@gmail.com>
2016-09-17 14:31:41 +0800

    for centerline

--- public/js/potrace.js
+++ public/js/potrace.js
@@ -1278,9 +1278,8 @@ var Potrace = (function() {
 
     var w = bm.w * size, h = bm.h * size,
       len = pathlist.length, c, i, strokec, fillc, fillrule;
-
-    var svg = '<svg id="svg" version="1.1" width="' + w + '" height="' + h +
-        '" xmlns="http://www.w3.org/2000/svg">';
+////width="' + w + '" height="' + h 
+    var svg = '<svg id="svg" version="1.1" xmlns="http://www.w3.org/2000/svg">';
     svg += '<path d="';
     for (i = 0; i < len; i++) {
       c = pathlist[i].curve;
